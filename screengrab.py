303493c21cde7810ab7b3579128db1f0e009a371
doggyisnice <119186374+doggyisnice@users.noreply.github.com>
2023-01-21 16:46:23 +0800

    Screengrab.py
    Added new feature for saving the screenshot filename as the time it was taken

--- screengrab.py
+++ screengrab.py
@@ -1,6 +1,15 @@
-from PIL import ImageGrab
+import pyautogui
+import time
+from time import time, ctime
 
-myscreen = ImageGrab.grab()
-myscreen.save('myscreen_PIL.png')
+def take_screenshot():
+  t = time()  
+  file_name = ctime(t) + ".png" 
+  path_name = '/Users/joel/Desktop/Screenshots/'
 
+  myScreenshot = pyautogui.screenshot()
+  myScreenshot.save(path_name + file_name)
+  
+
+take_screenshot()
 
